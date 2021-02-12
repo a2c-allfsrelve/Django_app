597ac6c01e17294e2a0f85d63166a02afe9e09f1
LamB <a2c.ambivalent@outlook.jp>
2021-02-13 00:22:11 +0900

    p66-List2-8

---
+++ hello/views.py
@@ -0,0 +1,15 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    if 'msg' in request.GET:
+        msg = request.GET['msg']
+        result = 'You typed  "'+ msg + '"'
+    else:
+        result = 'Prease send your msg.'
+
+    return HttpResponse(result)
+
+
